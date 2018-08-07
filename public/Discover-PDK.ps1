ed678893ecb931d16153b9b71ac91e42290daa6d
Mike Whitehead <mike.whitehead@nchs-health.org>
2018-08-07 14:57:56 -0700

    Added Discover-PDK.ps1 which locates the endpoint URL's for PDK

--- public/Discover-PDK.ps1
+++ public/Discover-PDK.ps1
@@ -1,10 +1,29 @@
 function Discover-PDK {
-$configurationEndpoint = "https://accounts.pdk.io/.well-known/openid-configuration"
+    Param(
+        [Parameter(Mandatory=$True)]
+        [ValidateSet("dev","prod")]
+        [string[]]$Environment
+    )
 
-$global:configuration = Invoke-RestMethod -Method Get -Uri $configurationEndpoint
+    switch ($environment) {
+        dev { $credPath = "$PSScriptRoot\..\private\credentials_dev.json" }
+        prod { $credPath = "$PSScriptRoot\..\private\credentials.json" }
+        Default { $credPath = "$PSScriptRoot\..\private\credentials.json" }
+    }
 
-$global:authEndpoint = $configuration.authorization_endpoint
-$global:tokenEndpoint = $configuration.token_endpoint
+    $configurationEndpoint = "https://accounts.pdk.io/.well-known/openid-configuration"
+
+    $creds = Get-Content $credPath | ConvertFrom-Json
+    $global:clientId = $creds.clientId
+    $global:clientSecret = $creds.clientSecret
+    $global:configuration = Invoke-RestMethod -Method Get -Uri $configurationEndpoint
+
+    $global:authEndpoint = $configuration.authorization_endpoint
+    $global:tokenEndpoint = $configuration.token_endpoint
+
+    "clientId=$global:clientId"
+    "authorization_endpoint=$global:authEndpoint"
+    "token_endpoint=$global:tokenEndpoint"
 }
 
 Export-ModuleMember -Function 'Discover-PDK'
