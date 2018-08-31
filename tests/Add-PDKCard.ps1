0e1044cab92d438a3ea259cb78d0c2955734243a
Mike Whitehead <mike.whitehead@nchs-health.org>
2018-08-30 17:23:38 -0700

    Did cleanup, added files/folders to gitignore

--- tests/Add-PDKCard.ps1
+++ tests/Add-PDKCard.ps1
@@ -4,10 +4,10 @@ Import-Module .\PS-ProDataKey.psm1
 
 $PDKClientId = '5b60600f9f83fc6c36cf1021'
 $PDKClientSecret = 'AO1cXvXq7dFKPy3Haw4edWfNpuDj5omy'
-$PDKPanelId = '1070DLI'
-$PDKUserId = $PDKUser.id
+$PDKPanelId = '1070E67'
+$PDKUserId = 106
 
-$PDKCardNumber = "12569"
+$PDKCardNumber = "22222"
 $PDKCardDescription = "Test Card"
 
 Add-PDKCard -PDKClientId $PDKClientId -PDKClientSecret $PDKClientSecret -PDKPanelId $PDKPanelId -PDKUserId $PDKUserId -PDKCardNumber $PDKCardNumber -PDKCardDescription $PDKCardDescription
